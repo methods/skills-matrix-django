5abfad5687589d8bb922ac2be11d6580b49c9d7b
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-06-15 16:16:11 +0100

    landing page button passes test

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/edit-skills', views.edit_skills, name='edit skills')
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "app/home.html")
+
+
+def edit_skills(request):
+    return render(request, "app/edit_skills.html")
