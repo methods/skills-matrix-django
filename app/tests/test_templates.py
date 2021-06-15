6656642bdf1a5ee1d2973032d26e6ebb7abd6362
Valentina <vmilitaru28@gmail.com>
2021-06-15 14:34:45 +0100

    testing the body resp

--- app/tests/test_templates.py
+++ app/tests/test_templates.py
@@ -5,3 +5,7 @@ class SimpleTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_home_page_body_resp(self):
+        response = self.client.get('/')
+        self.assertTrue("Welcome" in response.content)
