50150639b16c84048c2f4585be68f17dec682430
Valentina <vmilitaru28@gmail.com>
2021-07-01 09:07:57 +0100

    updates the email invalid error message and updates the email validator function to run only after the user is inputing a valid email

--- signup/forms.py
+++ signup/forms.py
@@ -34,12 +34,14 @@ class JobForm(forms.Form):
 
 class EmailForm(forms.Form):
     email_address = forms.EmailField(validators=[validate_domain_email],label='Email address', max_length=100,
-                                     widget=GdsStyleEmailInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Please Enter Your Email Address'})
+                                     widget=GdsStyleEmailInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Please Enter Your Email Address','invalid':"Pleae enter an email address in the correct format, like name@example.com"})
 
 
     def __init__(self, *args, **kwargs):
         super(EmailForm, self).__init__(*args, **kwargs,use_required_attribute=False)
         attrs = {}
+        print(self.fields['email_address'].error_messages)
+        print(self.errors)
         attrs.update({"errors": True})
         attrs['class'] = 'govuk-input--error'
         for field in self.fields:
