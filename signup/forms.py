c48dcbd8b1e520487c40a8ac4520420f70c7cb6d
Valentina <vmilitaru28@gmail.com>
2021-07-01 11:37:51 +0100

    updates the error messages letter casing

--- signup/forms.py
+++ signup/forms.py
@@ -7,9 +7,9 @@ from .validators import validate_domain_email
 
 class NameForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=100,
-                                 widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Please Enter Your First Name'})
+                                 widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Enter your first name'})
     surname = forms.CharField(label='Surname', max_length=100,
-                              widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Please Enter Your Surname'})
+                              widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Enter your surname'})
 
     def __init__(self, *args, **kwargs):
         super(NameForm, self).__init__(*args, **kwargs)
@@ -30,7 +30,7 @@ class JobForm(forms.Form):
     
 class EmailForm(forms.Form):
     email_address = forms.EmailField(validators=[validate_domain_email],label='Email address', max_length=100,
-                                     widget=GdsStyleEmailInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Please Enter Your Email Address','invalid':"Pleae enter an email address in the correct format, like name@example.com"})
+                                     widget=GdsStyleEmailInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Enter your email address.','invalid':"Enter an email address in the correct format, like name@example.com"})
 
 
     def __init__(self, *args, **kwargs):
@@ -48,9 +48,9 @@ class EmailForm(forms.Form):
 class PasswordForm(forms.Form):
     password = forms.CharField(widget=GdsStylePasswordInput(), label='You will use this password to '
                                                              'log in securely to the platform.',
-                               min_length=8, max_length=25, error_messages={'required': 'Please Enter Your Password'})
+                               min_length=8, max_length=25, error_messages={'required': 'Enter your password'})
     password_confirm = forms.CharField(widget=GdsStylePasswordInput(), label='Confirm password',min_length=8, max_length=25,
-                                       error_messages={'required': 'Please Confirm Your Password'})
+                                       error_messages={'required': 'Confirm your password'})
 
     def clean_password_confirm(self):
         password = self.cleaned_data.get('password')
@@ -60,7 +60,7 @@ class PasswordForm(forms.Form):
         return password_confirm
     
     def __init__(self, *args, **kwargs):
-        super(PasswordForm, self).__init__(*args, **kwargs,use_required_attribute=False)
+        super(PasswordForm, self).__init__(*args, **kwargs)
         attrs = {}
         attrs.update({"errors": True})
         attrs['class'] = 'govuk-input--error'
--- signup/validators.py
+++ signup/validators.py
@@ -6,5 +6,5 @@ regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 def validate_domain_email(value):
     if(re.match(regex, value)):
         if not "methods.co.uk" in value.split('@', 1)[-1]:
-            raise ValidationError("Please enter your Methods email address in the correct format, like firstname.surname@methods.co.uk.")
+            raise ValidationError("Enter your Methods email address in the correct format, like firstname.surname@methods.co.uk")
     return value
