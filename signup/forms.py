2b4c1df90d70f1dd5314c2347888be3546c61505
Valentina <vmilitaru28@gmail.com>
2021-07-01 11:29:48 +0100

    updates the password creation error messages

--- signup/forms.py
+++ signup/forms.py
@@ -60,10 +60,10 @@ class PasswordForm(forms.Form):
         return password_confirm
     
     def __init__(self, *args, **kwargs):
-        super(PasswordForm, self).__init__(*args, **kwargs)
+        super(PasswordForm, self).__init__(*args, **kwargs,use_required_attribute=False)
         attrs = {}
         attrs.update({"errors": True})
         attrs['class'] = 'govuk-input--error'
-        for field in self.fields:
-            if field in self.errors:  
+        if self.errors:
+            for field in self.fields:
                 self.fields[field].widget = GdsStylePasswordInput(attrs=attrs)
