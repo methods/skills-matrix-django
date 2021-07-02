6f96a71f827b55062f031de6e11193813b6dcfe0
Valentina <vmilitaru28@gmail.com>
2021-07-02 18:03:02 +0100

    adds the superuser,but you can't log in with a normal user anymore

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,27 +1,58 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 
 class CustomAccountManager(BaseUserManager):
-    def create_user(self, email, first_name, surname, team, job_role, password):
+
+    def create_superuser(self, email, first_name, surname, team, job_role, password, **other_fields):
+
+        other_fields.setdefault('is_staff',True)
+        other_fields.setdefault('is_superuser',True)
+        other_fields.setdefault('is_admin',True)
+        
+            
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must be assigned to is_supersuser=True.')
+        
+        if other_fields.get('is_admin') is not True:
+            raise ValueError('Superuser must be assigned to is_admin=True.')
+        
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must be assigned to is_staff=True.')
+
+        return self.create_user(email=self.normalize_email(email), first_name=first_name, surname=surname, team=team, job_role=job_role, password=password, **other_fields)
+
+    def create_user(self, email, first_name, surname, team, job_role, password,**other_fields):
+
+        if not email:
+            raise ValueError('You must provide and email address')
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, surname=surname, team=team, job_role=job_role,
-                          password=password)
+        user = self.model(email=email, first_name=first_name, surname=surname, team=team, job_role=job_role,**other_fields)
+        user.set_password(password)
         user.save()
         return user
 
+    
 
-class NewUser(AbstractBaseUser):
+class NewUser(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     team = models.CharField(max_length=100)
     job_role = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login',auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+    is_staff = models. BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = CustomAccountManager()
 
     USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS=['first_name','surname','team', 'job_role']
+
 
     def __str__(self):
         return f"{self.email}, {self.first_name}, {self.team}, {self.job_role}"
