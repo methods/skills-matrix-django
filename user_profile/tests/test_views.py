1cfa5134439bb2b9a92d1a176c3427a2944b6f9c
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-07-02 14:44:31 +0100

    tests logout view

--- user_profile/tests/test_views.py
+++ user_profile/tests/test_views.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 
 class LoginPageTests(TestCase):
     def test_login_page_GET(self):
-        response = self.client.get('/')
+        response = self.client.get('')
         assert response.status_code == 200
         self.assertTemplateUsed(response, 'user_profile/login.html')
 
+
+class LogoutPageTests(TestCase):
+    def test_logout_page_GET(self):
+        response = self.client.get('/logout')
+        assert response.status_code == 200
+        self.assertTemplateUsed(response, 'user_profile/logout.html')
+
+    def test_user_logout(self):
+        response = self.client.post('/logout', {})
+        assert response.status_code == 302
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import views as auth_views
 from .forms import LoginForm
+from django.contrib.auth import logout as django_logout, get_user_model
+from django.shortcuts import redirect, render
 
 
 class LoginView(auth_views.LoginView):
@@ -7,3 +9,10 @@ class LoginView(auth_views.LoginView):
     template_name = 'user_profile/login.html'
     redirect_field_name = 'dashboard/'
     redirect_authenticated_user = True
+
+
+def logout(request):
+    if request.method == 'POST':
+        django_logout(request)
+        return redirect('/')
+    return render(request, 'user_profile/logout.html')
