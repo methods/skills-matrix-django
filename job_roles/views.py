e7d876de008bce86f2f86a8b762063fc60c7b959
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-07-08 16:24:54 +0100

    adds super admin filter to add job role view

--- job_roles/views.py
+++ job_roles/views.py
@@ -8,6 +8,7 @@ def job_roles(request):
 
 
 @login_required
-@user_passes_test(lambda u: u.groups.filter(name='Admins').exists(), login_url='/error/not-authorised')
+@user_passes_test(lambda u: u.groups.filter(name='Admins').exists() or u.groups.filter(name='Super admins').exists(),
+                  login_url='/error/not-authorised')
 def add_job_role(request):
     return render(request, "job_roles/add_job_role.html")
