8974d0e149caa3808327fedebdfaea3495e6ad45
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-07-09 11:42:11 +0100

    fixes app view testing with logged in user

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,17 +1,22 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 
 class GeneralFunctionalTests(TestCase):
 
     def setUp(self):
         self.User = get_user_model()
-        self.user = self.User.objects.create_user('test@methods.co.uk', 'Test', 'Test', 'OPC',
-                                                  'Junior Developer', 'password')
+        password = make_password('password')
+        self.user = self.User.objects.create_user('test@methods.co.uk', 'test_first_name', 'test_surname', 'OPC',
+                                                  'Junior Developer', password)
         self.client.login(username='test@methods.co.uk', password='password')
 
+    def tearDown(self):
+        self.User = get_user_model()
+        self.User.objects.all().delete()
+
     def test_home_page_status_code(self):
-        # breakpoint()
         response = self.client.get('/dashboard')
         assert response.status_code == 200
 
@@ -19,15 +24,7 @@ class GeneralFunctionalTests(TestCase):
         response = self.client.get('/dashboard')
         assert "Welcome" in response.content.decode()
 
-    # def test_edit_skills_button(self):
-    #     self.client.get('/dashboard')
-    #     button = self.client.find_element_by_class_name('govuk-button')
-    #     button.click()
-    #     assert '/edit-skills' == self.client.current_url
-
     def test_user_details(self):
-        db = get_user_model()
-        db.objects.create_user('ss@user.com', 'userfirstname', 'usersurname', 'userteam', 'userjobrole', 'password')
         response = self.client.get('/dashboard')
-        assert "userfirstname" in response.content.decode()
-        assert "usersurname" in response.content.decode()
+        assert "test_first_name" in response.content.decode()
+        assert "test_surname" in response.content.decode()
