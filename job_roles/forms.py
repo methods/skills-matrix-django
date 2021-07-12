7a427519772c592401048f1e0301f3cce84e23e8
Valentina <vmilitaru28@gmail.com>
2021-07-12 16:41:08 +0100

    creates and tests the JobSkillsAndSkillLevelForm

--- job_roles/forms.py
+++ job_roles/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from signup.widgets import GdsStyleTextInput
+from app.models import Skill, SkillLevel
 from .validators import validate_input_capitalised
 
 
@@ -17,3 +18,17 @@ class JobTitleForm(forms.Form):
         for field in self.fields:
             if field in self.errors:
                 self.fields[field].widget = GdsStyleTextInput(attrs=attrs)
+
+
+class JobSkillsAndSkillLevelForm(forms.Form):
+    def get_skill_choices():
+        skill_options = [(skill.name, skill.name) for skill in Skill.objects.all()]
+        return skill_options
+
+    def get_skill_level_choices():
+        skill_level_options = [(skill_level.name, skill_level.name) for skill_level in SkillLevel.objects.all()]
+        return skill_level_options
+
+    job_role_skill = forms.ChoiceField(choices=get_skill_choices, widget=forms.Select(attrs={'class': 'govuk-select'}))
+    job_role_skill_level = forms.ChoiceField(choices=get_skill_level_choices,
+                                             widget=forms.Select(attrs={'class': 'govuk-select'}))
--- signup/forms.py
+++ signup/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 from super_admin.models import Team
 from job_roles.models import Job
-from signup.widgets import GdsStylePasswordInput,GdsStyleTextInput,GdsStyleEmailInput
+from signup.widgets import GdsStylePasswordInput, GdsStyleTextInput, GdsStyleEmailInput
 from .validators import validate_domain_email
 
 
 class NameForm(forms.Form):
     first_name = forms.CharField(label='First Name', max_length=100,
-                                 widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Enter your first name'})
+                                 widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),
+                                 error_messages={'required': 'Enter your first name'})
     surname = forms.CharField(label='Surname', max_length=100,
-                              widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Enter your surname'})
+                              widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),
+                              error_messages={'required': 'Enter your surname'})
 
     def __init__(self, *args, **kwargs):
         super(NameForm, self).__init__(*args, **kwargs)
@@ -17,25 +19,27 @@ class NameForm(forms.Form):
         attrs.update({"errors": True})
         attrs['class'] = 'govuk-input--error'
         for field in self.fields:
-            if field in self.errors:  
+            if field in self.errors:
                 self.fields[field].widget = GdsStyleTextInput(attrs=attrs)
 
 
 class JobForm(forms.Form):
     def get_team_choices():
-        team_options=[(team.team_name, team.team_name) for team in Team.objects.all()]
+        team_options = [(team.team_name, team.team_name) for team in Team.objects.all()]
         return team_options
+
     def get_job_choices():
         job_options = [(job.job_title, job.job_title) for job in Job.objects.all()]
         return job_options
-    team = forms.ChoiceField(choices=get_team_choices,widget=forms.Select(attrs={'class': 'govuk-select'}))
-    job = forms.ChoiceField(choices=get_job_choices,widget=forms.Select(attrs={'class': 'govuk-select'}))
+    team = forms.ChoiceField(choices=get_team_choices, widget=forms.Select(attrs={'class': 'govuk-select'}))
+    job = forms.ChoiceField(choices=get_job_choices, widget=forms.Select(attrs={'class': 'govuk-select'}))
 
-    
+
 class EmailForm(forms.Form):
-    email_address = forms.EmailField(validators=[validate_domain_email],label='Email address', max_length=100,
-                                     widget=GdsStyleEmailInput(attrs={'class': 'govuk-input'}),error_messages={'required': 'Enter your email address.','invalid':"Enter an email address in the correct format, like name@example.com"})
-
+    email_address = forms.EmailField(validators=[validate_domain_email], label='Email address', max_length=100,
+                                     widget=GdsStyleEmailInput(attrs={'class': 'govuk-input'}),
+                                     error_messages={'required': 'Enter your email address.',
+                                     'invalid': "Enter an email address in the correct format, like name@example.com"})
 
     def __init__(self, *args, **kwargs):
         super(EmailForm, self).__init__(*args, **kwargs)
@@ -43,15 +47,17 @@ class EmailForm(forms.Form):
         attrs.update({"errors": True})
         attrs['class'] = 'govuk-input--error'
         for field in self.fields:
-            if field in self.errors:  
+            if field in self.errors:
                 self.fields[field].widget = GdsStyleEmailInput(attrs=attrs)
 
 
 class PasswordForm(forms.Form):
-    password = forms.CharField(widget=GdsStylePasswordInput(), label='You will use this password to '
-                                                             'log in securely to the platform.',
-                               min_length=8, max_length=25, error_messages={'required': 'Enter your password'})
-    password_confirm = forms.CharField(widget=GdsStylePasswordInput(), label='Confirm password',min_length=8, max_length=25,
+    password = forms.CharField(widget=GdsStylePasswordInput(),
+                               label='You will use this password to log in securely to the platform.',
+                               min_length=8, max_length=25,
+                               error_messages={'required': 'Enter your password'})
+    password_confirm = forms.CharField(widget=GdsStylePasswordInput(), label='Confirm password', min_length=8,
+                                       max_length=25,
                                        error_messages={'required': 'Confirm your password'})
 
     def clean_password_confirm(self):
@@ -60,7 +66,7 @@ class PasswordForm(forms.Form):
         if password != password_confirm:
             raise forms.ValidationError('Passwords must match')
         return password_confirm
-    
+
     def __init__(self, *args, **kwargs):
         super(PasswordForm, self).__init__(*args, **kwargs)
         attrs = {}
