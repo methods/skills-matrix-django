61455d9ed7a6c491f6f5b3c3f6986a56ed9c9c29
Valentina <vmilitaru28@gmail.com>
2021-07-13 11:49:57 +0100

    displays the selected skills and associated skill levels into the add-job-role-skills template  and saves them into the session

--- job_roles/views.py
+++ job_roles/views.py
@@ -23,14 +23,22 @@ def add_job_role(request):
     return render(request, "job_roles/add_job_role.html", {'form': form})
 
 
+new_added_job_competencies = []
+
+
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='Admins').exists() or u.groups.filter(name='Super admins').exists(),
+                  login_url='/error/not-authorised')
 def add_job_role_skills(request):
     if request.method == 'POST':
         form = JobSkillsAndSkillLevelForm(request.POST)
         if form.is_valid():
             request.session['job_role_skill'] = request.POST['job_role_skill']
             request.session['job_role_skill_level'] = request.POST['job_role_skill_level']
+            new_added_job_competencies.append({request.POST['job_role_skill']: request.POST['job_role_skill_level']})
+            request.session['new_added_job_competencies'] = new_added_job_competencies
             request.session.save()
-            return redirect(new_job_role)
+            render(request, "job_roles/add_job_role_skills.html", {'form': form})
     else:
         form = JobSkillsAndSkillLevelForm()
     return render(request, "job_roles/add_job_role_skills.html", {'form': form})
