328e703abb739ea8670ba9907a4c2cffad444909
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-07-15 15:54:31 +0100

    adds add skill level functionality

--- super_admin/forms.py
+++ super_admin/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from .widgets import GdsStylePasswordInput,GdsStyleTextInput,GdsStyleEmailInput
+from common.widgets import GdsStylePasswordInput,GdsStyleTextInput,GdsStyleEmailInput
 
 
 class SkillLevelForm(forms.Form):
     name = forms.CharField(label='Skill level', max_length=100,
                                  widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}),
                            error_messages={'required': 'Enter name of skill level'})
-    description = forms.CharField(label='Description', max_length=1000,
+    description = forms.CharField(label='Description (optional)', max_length=1000, required=False,
                               widget=GdsStyleTextInput(attrs={'class': 'govuk-input'}))
 
     def __init__(self, *args, **kwargs):
--- super_admin/views.py
+++ super_admin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import user_passes_test, login_required
 from .models import SkillLevel
 from .forms import SkillLevelForm
@@ -22,5 +22,12 @@ def view_skill_levels(request):
 @user_passes_test(lambda u: u.groups.filter(name='Super admins').exists(),
                   login_url='/error/not-authorised')
 def add_skill_level(request):
+    if request.POST:
+        form = SkillLevelForm(request.POST)
+        if form.is_valid():
+            name = request.POST['name']
+            description = request.POST['description']
+            SkillLevel.objects.create(name=name, description=description)
+            return redirect(view_skill_levels)
     form = SkillLevelForm()
     return render(request, 'super_admin/add_skill_level.html', {'form': form})
