15059eba727a61d50b376d6a8002b13f62b159e7
Valentina <vmilitaru28@gmail.com>
2021-07-21 17:39:15 +0100

    fixes the update_job_role template render issue

--- common/widgets.py
+++ common/widgets.py
@@ -1,8 +1,10 @@
 from django.forms.widgets import Input
 
+
 class GdsStylePasswordInput(Input):
-    template_name="user_management/gds_input.html"
+    template_name = "user_management/gds_input.html"
     input_type = 'password'
+
     def __init__(self, attrs=None, render_value=False):
         super().__init__(attrs)
         self.render_value = render_value
@@ -12,9 +14,11 @@ class GdsStylePasswordInput(Input):
             value = None
         return super().get_context(name, value, attrs)
 
+
 class GdsStyleTextInput(Input):
-    template_name="user_management/gds_input.html"
+    template_name = "user_management/gds_input.html"
     input_type = 'text'
+
 
 class GdsStyleEmailInput(Input):
     input_type = 'email'
