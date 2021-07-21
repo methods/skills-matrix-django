178d1d6e9fdb7034c54bbd54b7ddabb89ffaea18
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-07-21 14:32:15 +0100

    fixes url issues in job role tests

--- job_roles/tests/test_views.py
+++ job_roles/tests/test_views.py
@@ -20,23 +20,25 @@ class AddJobRolePageTests(LoggedInUserTestCase):
         self.group.save()
 
     def test_page_GET_non_admin_users(self):
-        response = self.client.get(reverse('add-a-job-role'))
+        response = self.client.get(reverse('add-job-title'))
         assert response.status_code == 302
 
     def test_page_GET_admin_users(self):
         admins_group = Group.objects.get(name='Admins')
         self.user.groups.add(admins_group)
-        response = self.client.get(reverse('add-a-job-role'))
+        response = self.client.get(reverse('add-job-title'))
         assert response.status_code == 200
         self.assertTemplateUsed(response, 'job_roles/add_job_role.html')
 
     def test_job_role_title_saved_in__session(self):
-        self.adds_admins_group_to_users()
-        self.client.post('/job-roles/add-job-role-title', {'job_role_title': 'Senior Developer'})
+        admins_group = Group.objects.get(name='Admins')
+        self.user.groups.add(admins_group)
+        self.client.post(reverse('add-job-title'), {'job_role_title': 'Senior Developer'})
         session = self.client.session
         self.assertEqual(session['job_role_title'], 'Senior Developer')
 
     def test_valid_submission_redirects_to_add_job_role_skills_page(self):
-        self.adds_admins_group_to_users()
-        response = self.client.post('/job-roles/add-job-role-title', {'job_role_title': 'Lead Developer'})
-        self.assertRedirects(response, '/job-roles/add-job-role-skills')
+        admins_group = Group.objects.get(name='Admins')
+        self.user.groups.add(admins_group)
+        response = self.client.post(reverse('add-job-title'), {'job_role_title': 'Lead Developer'})
+        self.assertRedirects(response, '/job-roles/add-job-role-skills/')
