efb1c4415276ad8323e434c074a5aace02f4ed0b
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-08-04 11:26:34 +0100

    adds select option to team and job form fields, refactors custom select widget into common app

--- common/widgets.py
+++ common/widgets.py
@@ -1,4 +1,5 @@
 from django.forms.widgets import Input
+from django.forms import Select
 
 
 class GdsStylePasswordInput(Input):
@@ -23,3 +24,25 @@ class GdsStyleTextInput(Input):
 class GdsStyleEmailInput(Input):
     input_type = 'email'
     template_name = 'user_management/gds_input.html'
+
+
+class CustomisedSelectWidget(Select):
+    def __init__(self, *args, disabled_choices=[], **kwargs):
+        self._disabled_choices = disabled_choices
+        super(CustomisedSelectWidget, self).__init__(*args, **kwargs)
+
+    @property
+    def disabled_choices(self):
+        return self._disabled_choices
+
+    @disabled_choices.setter
+    def disabled_choices(self, other):
+        self._disabled_choices = other
+
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option_dict = super(CustomisedSelectWidget, self).create_option(
+            name, value, label, selected, index, subindex=subindex, attrs=attrs
+        )
+        if value in self.disabled_choices:
+            option_dict['attrs']['disabled'] = True
+        return option_dict
--- job_roles/form_utils.py
+++ job_roles/form_utils.py
@@ -1,6 +1,7 @@
 from super_admin.models import SkillLevel
 from app.models import Skill
 
+
 def get_skill_choices():
     skill_options = [(skill.name, skill.name) for skill in Skill.objects.all()]
     skill_options.insert(0, ('', '--Select a skill--'))
