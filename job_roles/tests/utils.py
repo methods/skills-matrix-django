1c12a98d32179eca0f8983a756758e8098206c6a
Valentina <vmilitaru28@gmail.com>
2021-08-04 11:06:06 +0100

    fixes the test_job_role_skills_form_valid_data failing test

--- job_roles/tests/utils.py
+++ job_roles/tests/utils.py
@@ -11,11 +11,18 @@ def creates_job_competency_instances():
     return {'test_job': test_job, 'test_skill': test_skill, 'test_skill_level': test_skill_level}
 
 
+def creates_job_role_skill_and_skill_level_instances():
+    Skill.objects.create(name='skill_1', skill_type='Career skill')
+    Skill.objects.create(name='skill_2', skill_type='Career skill')
+    Skill.objects.create(name='skill_3', skill_type='Career skill')
+    Skill.objects.create(name='skill_4', skill_type='Career skill')
+    SkillLevel.objects.create(name='skill_level_2')
+    SkillLevel.objects.create(name='skill_level_1')
+    SkillLevel.objects.create(name='skill_level_4')
+    SkillLevel.objects.create(name='skill_level_3')
+
+
 def creates_job_role_skill_and_skill_level_form(form_data=None, disabled_choices=None):
-    skill_options = [('skill_1', 'skill_1'), ('skill_2', 'skill_2'),('skill_3', 'skill_3')]
-    skill_level_options = [('skill_level_1', 'skill_level_1'), ('skill_level_2', 'skill_level_2'),
-                           ('skill_level_3', 'skill_level_3')]
+    creates_job_role_skill_and_skill_level_instances()
     job_role_skills_form = JobSkillsAndSkillLevelForm(data=form_data, disabled_choices=disabled_choices)
-    job_role_skills_form.fields['job_role_skill_level'].choices = skill_level_options
-    job_role_skills_form.fields['job_role_skill'].choices = skill_options
     return job_role_skills_form
