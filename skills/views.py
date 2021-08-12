50e22cf114e8db8aada922c36e5f9153befcebce
Valentina <vmilitaru28@gmail.com>
2021-08-12 10:42:52 +0100

    adds Team model to the project admin dashboard and replaces the filter method with get in the delete function

--- skills/views.py
+++ skills/views.py
@@ -11,7 +11,7 @@ class ViewSkillsView(AdminUserMixin, CustomView):
         return render(request, 'skills/view_skills.html', {'skills': skills})
 
     def delete(self, request):
-        Skill.objects.filter(pk=request.POST['delete']).delete()
+        Skill.objects.get(pk=request.POST['delete']).delete()
         skills = Skill.objects.order_by('name')
         return render(request, 'skills/view_skills.html', {'skills': skills})
 
