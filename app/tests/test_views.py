eeeaaef46dd651a53cf19aabafec1ff7a131bac8
Valentina <vmilitaru28@gmail.com>
2021-08-16 16:48:33 +0100

    adds 2 more tests to the DashboardTests class to check if the skills/skill levels and no job role skills  message renders as expected

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -1,6 +1,6 @@
 from common.tests.custom_classes import LoggedInUserTestCase
 from django.urls import reverse
-from .utils import creates_job_role_title_instance
+from .utils import creates_job_role_title_instance, creates_job_role_competency_instance
 
 
 class DashboardTests(LoggedInUserTestCase):
@@ -22,6 +22,20 @@ class DashboardTests(LoggedInUserTestCase):
         assert "OPC" in response.content.decode()
         assert "Junior Developer" in response.content.decode()
         self.assertNotIn('test_surname', response.content.decode())
+
+    def test_message_renders_when_no_skills(self):
+        creates_job_role_title_instance()
+        response = self.client.get(reverse('dashboard'))
+        self.assertIn('Sorry, but at the moment the Junior Developer role has no skills assigned to.',
+                      response.content.decode())
+
+    def test_skill_skill_levels_renders(self):
+        creates_job_role_competency_instance()
+        response = self.client.get(reverse('dashboard'))
+        assert "test_skill_1" in response.content.decode()
+        assert "test_skill_level_1" in response.content.decode()
+        self.assertNotIn('Sorry, but at the moment the Junior Developer role has no skills assigned to.',
+                         response.content.decode())
 
 
 class BrowseProfileTests(LoggedInUserTestCase):
--- app/tests/utils.py
+++ app/tests/utils.py
@@ -1,6 +1,24 @@
-from job_roles.models import Job
+from job_roles.models import Job, Competency
+from super_admin.models import SkillLevel
+from skills.models import Skill
 
 
 def creates_job_role_title_instance():
-    test_job = Job.objects.create(job_title='Junior Developer')
-    return {'test_job_role': test_job}
+    test_job_title = Job.objects.create(job_title='Junior Developer')
+    return test_job_title
+
+
+def creates_skill_instance():
+    test_skill = Skill.objects.create(name='test_skill_1')
+    return test_skill
+
+
+def creates_skill_level_instance():
+    test_skill_level = SkillLevel.objects.create(name='test_skill_level_1')
+    return test_skill_level
+
+
+def creates_job_role_competency_instance():
+    Competency.objects.create(job_role_title=creates_job_role_title_instance(),
+                              job_role_skill=creates_skill_instance(),
+                              job_role_skill_level=creates_skill_level_instance())
