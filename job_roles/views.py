eb7cf14c150821f24a51d22d8e3ab871570e6d43
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-08-17 16:48:18 +0100

    removes large comment and refactors additional handle_form_errors function

--- job_roles/views.py
+++ job_roles/views.py
@@ -82,45 +82,6 @@ class AddJobRoleSkills(LoginRequiredMixin, AdminUserMixin, CustomView):
                                                                       'new_role': True})
 
 
-# @login_required
-# @user_passes_test(lambda u: u.groups.filter(name='Admins').exists() or u.groups.filter(name='Super admins').exists(),
-#                   login_url='/error/not-authorised')
-# def add_job_role_skills(request):
-#     if 'job_role_title' in request.session.keys():
-#         if 'disabled_choices' not in request.session.keys():
-#             request.session['disabled_choices'] = ['']
-#         form = JobSkillsAndSkillLevelForm(disabled_choices=request.session['disabled_choices'])
-#     else:
-#         messages.info(request, 'Please make sure to add a job role title.')
-#         return redirect('add-job-title')
-#     if request.method == 'POST':
-#         if 'delete' in request.POST.keys():
-#             if request.POST["delete"] in request.session['disabled_choices'] and len(
-#                     request.session['disabled_choices']) > 0:
-#                 request.session['disabled_choices'].remove(request.POST['delete'])
-#             for competency in request.session['new_added_job_competencies']:
-#                 if request.POST["delete"] in competency:
-#                     request.session['new_added_job_competencies'].remove(competency)
-#         else:
-#             if 'disabled_choices' in request.session.keys():
-#                 form = JobSkillsAndSkillLevelForm(request.POST, disabled_choices=request.session['disabled_choices'], request=request)
-#             else:
-#                 form = JobSkillsAndSkillLevelForm(request.POST, request=request)
-#             if 'new_added_job_competencies' not in request.session.keys():
-#                 request.session['new_added_job_competencies'] = []
-#             if 'addSkill' in request.POST.keys():
-#                 if form.is_valid():
-#                     form.process_session_save()
-#                     form = JobSkillsAndSkillLevelForm(disabled_choices=request.session['disabled_choices'])
-#                 else:
-#                     for field, errors in form.errors.items():
-#                         for error in errors:
-#                             messages.error(request, error)
-#     competencies = request.session['new_added_job_competencies'] if 'new_added_job_competencies' in request.session.keys() else []
-#     return render(request, "job_roles/add_job_role_skills.html", {'form': form, 'competencies': competencies,
-#                                                                   'new_role': True})
-
-
 @login_required
 @user_passes_test(lambda u: u.groups.filter(name='Admins').exists() or u.groups.filter(name='Super admins').exists(),
                   login_url='/error/not-authorised')
@@ -220,8 +181,6 @@ def add_a_skill(request, job_title):
             form.process(request.POST, job)
             return redirect(add_a_skill, job_title=job_title)
         else:
-            for field, errors in form.errors.items():
-                for error in errors:
-                    messages.error(request, error)
+            handle_form_errors(form, request)
     return render(request, "job_roles/add_job_role_skills.html", {'form': form, 'competencies': competencies_by_name,
                                                                   'job_title': job.job_title, 'existing_role': True})
