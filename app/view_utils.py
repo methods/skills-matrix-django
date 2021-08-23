c0661e1672d9d2315641db0fe118603dadd14e4f
Valentina <vmilitaru28@gmail.com>
2021-08-23 13:41:28 +0100

    updates the dashboard view so that is displaying  the job role competencies with the user skill level if the skill already exists in the user competency table and if not it renders only the message >>Enter your skill level<<

--- app/view_utils.py
+++ app/view_utils.py
@@ -2,8 +2,19 @@ from app.forms import UserSkillLevelForm
 from skills.models import UserCompetencies
 
 
-def prepare_competency_update(individual_job_related_competency_id):
-    individual_job_related_competency = UserCompetencies.objects.get(id=individual_job_related_competency_id)
-    form = UserSkillLevelForm(initial={'user_skill_level': individual_job_related_competency.skill_level.name})
-    update_indiviudal_competency_id = int(individual_job_related_competency_id)
-    return {'form': form, 'update_indiviudal_competency_id': update_indiviudal_competency_id}
+
+def prepare_competency_update(existing_user_skill_id, request):
+    if not UserCompetencies.objects.filter(skill=existing_user_skill_id, user=request.user.id).exists():
+        form = UserSkillLevelForm()
+    else:
+        individual_competency = UserCompetencies.objects.get(skill=existing_user_skill_id,user=request.user.id)
+        form = UserSkillLevelForm(initial={'user_skill_level': individual_competency.skill_level.name})
+    update_existing_skill_id = int(existing_user_skill_id)
+    return {'form':form, 'update_existing_skill_id': update_existing_skill_id}
+
+def retrieve_user_skills(all_user_competency,user_skills):
+    for user_competency in all_user_competency:
+        user_skills.append(user_competency.skill.id)
+    print(user_skills)
+    return user_skills
+    
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from job_roles.models import Competency, Job
 from skills.models import UserCompetencies, Skill
 from app.forms import UserSkillLevelForm
-from app.view_utils import prepare_competency_update
+from app.view_utils import prepare_competency_update,retrieve_user_skills
 from super_admin.models import SkillLevel
 from user_management.models import NewUser
 
@@ -12,30 +12,38 @@ from user_management.models import NewUser
 def dashboard(request):
     job_role_competency_list = Competency.objects.filter(job_role_title=Job.objects.get(job_title=request.user.job_role).id).order_by('id')
     individual_competency_list = UserCompetencies.objects.filter(job_role_related=False, user=request.user.id).order_by('id')
-    for competency in job_role_competency_list:
-        if not UserCompetencies.objects.filter(skill=competency.job_role_skill.id,user=request.user.id).exists():
-            UserCompetencies.objects.create(user=NewUser.objects.get(id=request.user.id),
-                                            skill=Skill.objects.get(id=competency.job_role_skill.id),
-                                            skill_level=SkillLevel.objects.get(name="Beginner"))
-        elif UserCompetencies.objects.filter(skill=competency.job_role_skill.id,user=request.user.id,job_role_related=False).exists():
-            UserCompetencies.objects.filter(skill=competency.job_role_skill.id,user=request.user.id,job_role_related=False).update(job_role_related=True)
+    all_user_competency = UserCompetencies.objects.filter(user=request.user.id).order_by('id')
+    user_skills = []
+    existing_skill_list=retrieve_user_skills(all_user_competency,user_skills)
+    # for competency in job_role_competency_list:
+    #     if not UserCompetencies.objects.filter(skill=competency.job_role_skill.id,user=request.user.id).exists():
+    #         UserCompetencies.objects.create(user=NewUser.objects.get(id=request.user.id),
+    #                                         skill=Skill.objects.get(id=competency.job_role_skill.id),
+    #                                         skill_level=SkillLevel.objects.get(name="Beginner"))
+    #     elif UserCompetencies.objects.filter(skill=competency.job_role_skill.id,user=request.user.id,job_role_related=False).exists():
+    #         UserCompetencies.objects.filter(skill=competency.job_role_skill.id,user=request.user.id,job_role_related=False).update(job_role_related=True)
     individual_job_related_competency_list = UserCompetencies.objects.filter(job_role_related=True, user=request.user.id).order_by('id')
-    for individual_job_related_competency in individual_job_related_competency_list:
-        if not Competency.objects.filter(job_role_title=Job.objects.get(job_title=request.user.job_role), job_role_skill=individual_job_related_competency.skill).exists():
-            UserCompetencies.objects.filter(skill=individual_job_related_competency.skill,user=request.user.id).update(job_role_related=False)
+    # for individual_job_related_competency in individual_job_related_competency_list:
+    #     if not Competency.objects.filter(job_role_title=Job.objects.get(job_title=request.user.job_role), job_role_skill=individual_job_related_competency.skill).exists():
+    #         UserCompetencies.objects.filter(skill=individual_job_related_competency.skill,user=request.user.id).update(job_role_related=False)
     if 'update-competency' in request.POST.keys():
-        template_variables = prepare_competency_update(request.POST['update-competency'])
+        template_variables = prepare_competency_update(request.POST['update-competency'], request)
         return render(request, "app/dashboard.html", {'form': template_variables['form'],
-                                                      'update_indiviudal_competency_id': template_variables['update_indiviudal_competency_id'],
+                                                      'update_existing_skill_id': template_variables['update_existing_skill_id'],
                                                       "individual_job_related_competency_list": individual_job_related_competency_list,
-                                                      "job_role_competency_list": job_role_competency_list})
+                                                      "job_role_competency_list": job_role_competency_list,
+                                                      "all_user_competency": all_user_competency,
+                                                      "user_skills": existing_skill_list})
     if 'save-skill-level' in request.POST.keys():
-        form = UserSkillLevelForm(request.POST)
-        if form.is_valid():
-            UserCompetencies.objects.filter(user=request.user.id, id=request.POST['save-skill-level']).update(skill_level=SkillLevel.objects.get(name=form.cleaned_data['user_skill_level']).id)
-    return render(request, "app/dashboard.html", {"job_role_competency_list": job_role_competency_list,
+        if not UserCompetencies.objects.filter(skill=request.POST['update-competency'],user=request.user.id).exists():
+            form = UserSkillLevelForm(request.POST)
+            if form.is_valid():
+                UserCompetencies.objects.filter(user=request.user.id, id=request.POST['save-skill-level']).update(skill_level=SkillLevel.objects.get(name=form.cleaned_data['user_skill_level']).id)
+    return render(request, "app/dashboard.html", {"job_role_competency_list": Competency.objects.filter(job_role_title=Job.objects.get(job_title=request.user.job_role).id).order_by('id'),
                                                   "individual_competency_list": individual_competency_list,
-                                                  "individual_job_related_competency_list": UserCompetencies.objects.filter(job_role_related=True, user=request.user.id).order_by('id'), "job_role_competency_list": job_role_competency_list})
+                                                  "individual_job_related_competency_list": UserCompetencies.objects.filter(job_role_related=True, user=request.user.id).order_by('id'), "job_role_competency_list": job_role_competency_list,
+                                                  "all_user_competency":all_user_competency,
+                                                  "user_skills":existing_skill_list})
 
 
 @login_required
